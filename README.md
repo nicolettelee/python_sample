4f4ff4370bbf09cbf64a79542b58b2288012fee0
Nicolette Lee <nicolette@Macbook>
2025-03-18 16:24:28 -0400

    Beginning to incorporate step 1: FastQC

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ python3 -m python_sample --infile /home/nicolettelee/infile.txt --outdir /home/n
 
 ## Outfiles
 
+
+## Notes on sample files used for testing
+Mouse WGS data was obtained from ENA, Project PRJNA316362, Run Accession SRR3476413.
+```
+curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR347/003/SRR3476413/SRR3476413_1.fastq.gz --output ./SRR3476413_1.fastq.gz
+curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR347/003/SRR3476413/SRR3476413_2.fastq.gz --output ./SRR3476413_2.fastq.gz
+```
+
--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 import argparse
 import time
 
-from .scripts import logging_funcs, preprocessing_funcs
+from .scripts import logging_funcs, preprocessing_funcs, util_funcs
 
 def main(options):
 
@@ -20,7 +20,46 @@ def main(options):
 
     # samplesheet checks
     samplesheet_df = preprocessing_funcs.samplesheet_checks(options.samplesheet, log_file)
-    print(samplesheet_df)
+    samples = list(samplesheet_df["Sample"])
+
+    logging_funcs.print_to_log(log_file, "info", f"Samples to be processed:")
+    logging_funcs.print_to_log(log_file, "info", samples)
+
+    ####################
+    # Step 1. FastQC on raw reads
+    ####################
+
+    logging_funcs.print_to_log(log_file, "info", f"Starting Step 1: FastQC on raw reads.")
+
+    # not parallelized for now
+    # construct commands
+    for sample in samples:
+        sample_row = samplesheet_df.loc[samplesheet_df["Sample"] == sample]
+        R1_read = sample_row["R1_File"][0]
+        R2_read = sample_row["R2_File"][0]
+    
+        fastqc_image_tag = options.fastqc_img
+        platform = options.platform
+
+        # construct dictionary for inputs & outputs
+        vols_dict = {}
+
+        # construct dictionary for general arguments
+        args_dict = {}
+
+        util_funcs.run_docker(fastqc_image_tag, platform, vols_dict, args_dict, log_file, invoke_cmd="fastqc")
+
+    ####################
+    # Step 2. Trim - implement later
+    ####################
+
+    ####################
+    # Step 3. FastQC on trimmed reads - implement later
+    ####################
+
+    ####################
+    # Step 4. Align
+    ####################
     
     # Finish script
     end_timer = time.perf_counter()
@@ -30,18 +69,31 @@ def main(options):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
+
+    ####################
+    # General parameters
+    ####################
+
     parser.add_argument("--samplesheet", action="store", type=str, required=True,
                         help="Path to samplesheet in string format.")
     parser.add_argument("--outdir", action="store", type=str, required=True,
                         help="Path to outdirectory in string format.")
     
-    ######
-    # Docker-specific commands
-    ######
+    ####################
+    # Docker-specific parameters
+    ####################
 
     parser.add_argument("--platform", action="store", required=False,
                         type=str, default="linux/amd64",
-                        help="String describing architecture types. Default: linux/amd64")
+                        help="String describing architecture type. Default: linux/amd64")
+
+    ####################
+    # Step 1-specific parameters
+    ####################
+
+    parser.add_argument("--fastqc_img", action="store", required=False,
+                        type=str, default="biocontainers/fastqc:v0.11.9_cv8",
+                        help="String in the format of [repository]:[tag] describing image to use for FastQC.s")
     
     options = parser.parse_args()
     main(options)
